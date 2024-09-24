1b42706d669a0a545edfb95be31b1ef48aabb7f2
MalisaK <malisa.khan@bumail.net>
2024-09-24 16:43:52 +0700

    Script Character

---
+++ Assets/Scripts/Lab/Croccodile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Croccodile : Enemy
+{
+    public override void Behaviour()
+    {
+        Debug.Log("Croccodile.Behaviour");
+    }
+}
